11da05b019637653fd76b94e6670e0890b89ed58
Net-Informatica <appnetinformatica@Mac-mini-di-Alessandro.local>
2021-02-08 17:57:49 +0100

    added ejs and post method

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 require("dotenv").config();
 
 const express = require("express");
+const mongoose = require("mongoose");
+const ShortUrl = require("./models/ShortUrl");
 const app = express();
 
+mongoose.connect("mongodb://localhost/urlShortener", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on("error", (err) => console.log(err));
+db.once("open", () => console.log("Connected to DB"));
+
+app.use(express.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.render("index");
+app.get("/", async (req, res) => {
+  const shortUrls = await ShortUrl.find();
+  res.render("index", {
+    shortUrls: shortUrls,
+  });
+});
+
+app.post("/shortUrls", async (req, res) => {
+  const newUrl = await ShortUrl.create({ full: req.body.fullUrl });
+  res.redirect("/");
 });
 
 app.listen(process.env.PORT || 5000, () =>
