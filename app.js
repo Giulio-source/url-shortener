df7a485ee691087358aa0cc633fcbf2391385654
Net-Informatica <appnetinformatica@Mac-mini-di-Alessandro.local>
2021-02-08 17:01:11 +0100

    initial setup

---
+++ app.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(process.env.PORT || 5000, () =>
+  console.log("Listening on port 5000")
+);
