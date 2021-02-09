4f376653036cb8fdb3c14f832b41c26a14a5e502
Net-Informatica <appnetinformatica@Mac-mini-di-Alessandro.local>
2021-02-09 09:54:28 +0100

    finished the redirection route

--- app.js
+++ app.js
@@ -25,7 +25,18 @@ app.get("/", async (req, res) => {
 
 app.post("/shortUrls", async (req, res) => {
   const newUrl = await ShortUrl.create({ full: req.body.fullUrl });
+
   res.redirect("/");
+});
+
+app.get("/:shortUrl", async (req, res) => {
+  const url = await ShortUrl.findOne({ short: req.params.shortUrl });
+  if (url == null) {
+    return res.sendStatus(404);
+  }
+  url.clicks++;
+  url.save();
+  res.redirect(url.full);
 });
 
 app.listen(process.env.PORT || 5000, () =>
